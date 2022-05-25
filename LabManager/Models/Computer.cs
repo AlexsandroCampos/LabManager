0c211e6a4ff56ceac958893341df585dd7cee43b
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-05-25 18:59:50 -0300

    fix: remove Lab route

--- LabManager/Models/Computer.cs
+++ LabManager/Models/Computer.cs
@@ -3,8 +3,8 @@ namespace LabManager.Models;
 class Computer
 {
     public int Id { get; set; }
-    public string? Ram { get; set; }
-    public string? Processor { get; set; }
+    public string Ram { get; set; }
+    public string Processor { get; set; }
 
     public Computer(int id, string ram, string processor)
     {
