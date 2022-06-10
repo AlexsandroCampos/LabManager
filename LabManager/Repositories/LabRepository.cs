9c2266bb8fa46119d3aa940d573d70021bf4db30
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-06-09 22:13:02 -0300

    feat: add lab repository class

---
+++ LabManager/Repositories/LabRepository.cs
@@ -0,0 +1,43 @@
+using LabManager.Models;
+using Microsoft.Data.Sqlite;
+using LabManager.Database;
+
+namespace LabManager.Repositories;
+
+class LabRepository
+{
+    private readonly DatabaseConfig _databaseConfig;
+
+    public LabRepository(DatabaseConfig databaseConfig)
+    {
+        _databaseConfig = databaseConfig;
+    }
+
+    public List<Lab> getAll()
+    {
+        var labs = new List<Lab>(); 
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Lab;";
+
+        var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            var lab = ReaderToComputer(reader);
+            labs.Add(lab);
+        }
+
+        reader.Close();
+        connection.Close();
+
+        return labs;
+    }
+
+    private Lab ReaderToLab(SqliteDataReader reader)
+    {
+        return new Lab(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2),  reader.GetString(3));
+    }
+}
